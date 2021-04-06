8bc60307141559c039af402552991d9ad5a2994c
songee-kim <songee-kim@nts-corp.com>
2021-04-06 22:10:16 +0900

    Minify code of dropdown in GNB

--- src/components/gnb/GnbDropdown.js
+++ src/components/gnb/GnbDropdown.js
@@ -1,35 +1,27 @@
 function GnbDropdown(props) {
-  const listSection = props.data.section.map(prop =>{
-    if ( props.data.section.length <= 1 ) {
+  const listSection = props.data.section.map(prop => {
+    const listItem = prop.item.map((prop2, idx) => {
       return (
-        prop.item.map((prop2, idx) => {
-          return (
-            <a key={ idx } href={ prop2.link } className="link_item">
-              <svg viewBox="0 0 24 24" className="svg_style" dangerouslySetInnerHTML={ {__html: prop2.icon} }></svg>
-              <span className="text">{ prop2.text }</span>
-            </a>
-          )
-        })
-      )} else {
-      return (
-        <div className="dropdown_section">
-          { prop.item.map((prop2, idx) =>{
-            return (
-              <a key={ idx } href={ prop2.link } className="link_item">
-                <svg viewBox="0 0 24 24" className="svg_style" dangerouslySetInnerHTML={ {__html: prop2.icon} }></svg>
-                <span className="text">{ prop2.text }</span>
-              </a>
-            )}
-          )}
-        </div>
-    )}
+        <a key={idx} href={prop2.link} className="link_item">
+          <svg viewBox="0 0 24 24" className="svg_style" dangerouslySetInnerHTML={{__html: prop2.icon}}></svg>
+          <span className="text">{prop2.text}</span>
+        </a>
+      );
+    });
+
+    return (
+      <>
+        { props.data.section.length < 2 && listItem }
+        { props.data.section.length >= 2 && <div className="dropdown_section">{ listItem }</div> }
+      </>
+    );
   });
 
   return (
     <div className={ props.data.classname }>
       { listSection }
     </div>
-  )
+  );
 }
 
 export default GnbDropdown;
